696ab8ed1797c348995f1a78c1aa69aebb5b4bed
Aidan Jones <aidan@aijo.info>
2021-04-18 13:22:01 -0700

    Now developers can use Bash script to replace instances of "*asap_s*"

---
+++ asap_s.sh
@@ -0,0 +1,17 @@
+if [ $# -eq 0 ]
+then
+  echo 'Please provide an argument.'
+  exit 2
+else
+  echo "Replacing…"
+fi
+
+find . -type f -name "*.php"     -exec sed -i '' "s/\'asap_s\'/\'$1\'/g" {} +
+find . -type f -name "*.php"     -exec sed -i '' "s/asap_s_/$1_/g" {} +
+find . -type f -name "*.php"     -exec sed -i '' "s/ asap_s/ $1/g" {} +
+find . -type f -name "*.php"     -exec sed -i '' "s/asap_s-/$1-/g" {} +
+find . -type f -name "*.css"     -exec sed -i '' "s/asap_s/$1/g" {} +
+find . -type f -name "style.css" -exec sed -i '' "s/Text Domain: asap_s/Text Domain: $1/g" {} +
+
+wait
+echo "String replacements complete"
