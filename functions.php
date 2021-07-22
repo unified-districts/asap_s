0ea1fd6f2aab964a91f8a43676c3acd83fb237e5
Aidan Jones <aidan@aijo.info>
2021-07-22 15:21:15 -0700

    LANGUAGES :: Removed localization support

--- functions.php
+++ functions.php
@@ -16,14 +16,6 @@ if ( ! function_exists( 'asap_s_setup' ) ) :
  * as indicating support for post thumbnails.
  */
 function asap_s_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on asap_s, use a find and replace
-	 * to change 'asap_s' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'asap_s', get_template_directory() . '/languages' );
-
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
