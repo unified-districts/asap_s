beb25fd4b6b99751bdeede9d437c889bb0cfc41b
Aidan Jones <aidan@aijo.info>
2021-07-23 14:53:16 -0700

    WEBPACK :: Added standard index.js file
    Default lazysizes, Font Awesome, and main.js import

---
+++ assets/scripts/index.js
@@ -0,0 +1,14 @@
+// Write code at the bottom of this file or import other JavaScript files for more modularity
+
+// LAZYSIZES
+import 'lazysizes/plugins/rias/ls.rias.js';
+import 'lazysizes/lazysizes.js';
+
+// FONT AWESOME. You must copy the kit's JavaScript into this file
+import './core/font-awesome.js';
+
+// MAIN. For general/global JavaScript
+import './main.js';
+
+// SWIPER
+// import Swiper from 'swiper';
